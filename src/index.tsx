498d5be756cf7e6eb78addab066058e7861a9fc6
Hugo <hugopatricio51@gmail.com>
2022-05-12 14:29:07 -0300

    Adding Miragejs initial config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs'
+
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          type: 'deposit'
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
