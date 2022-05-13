20c1a6821687441bee96a2806160e459d3d0ef8c
Hugo <hugopatricio51@gmail.com>
2022-05-13 15:12:39 -0300

    Adding switch button style and logic

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainter, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
     isOpen: boolean
@@ -8,6 +11,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+
+    const [type,setType] = useState('deposit')
+
     return ( 
         <Modal
         isOpen={isOpen}
@@ -24,6 +30,26 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
         <input placeholder='Título' />
         <input placeholder='Valor' type="number" />
+        <TransactionTypeContainter>
+            <RadioBox
+                type='button' 
+                onClick={() => setType('deposit')} 
+                isActive={ type == 'deposit'}
+            >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+            </RadioBox>
+
+            <RadioBox
+                type='button'  
+                onClick={() =>  setType('withdraw')}
+                isActive={ type == 'withdraw'}
+            >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+            </RadioBox>
+
+        </TransactionTypeContainter>
         <input placeholder='Categoria' />
         <button type="submit">Cadastrar</button>
         </Container>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -45,3 +45,44 @@ export const Container = styled.form`
     }
 
 `
+
+export const TransactionTypeContainter = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.5rem;
+
+`
+
+interface RadioBoxProps {
+    isActive: boolean
+}
+
+
+export const RadioBox = styled.button<RadioBoxProps>`
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+        background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        
+        &:hover {
+            border-color: #aaa ;
+        }
+
+        img { 
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+        }
+
+`
