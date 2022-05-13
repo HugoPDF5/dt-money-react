5a72268fb6ff8028a691c5d4e0398df23ed6d208
Hugo <hugopatricio51@gmail.com>
2022-05-13 16:53:14 -0300

    Adding correct formatting for displaying data in the table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable(){
 
+    interface Transaction {
+        id: number;
+        title: string;
+        amount: number;
+        type: string;
+        category: string;
+        createdAt: string;
+    }
+
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return (
@@ -22,24 +33,23 @@ export function TransactionsTable(){
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>ODSJFOJSDF</td>
-                        <td>1200</td>
-                        <td>SSASAD</td>
-                        <td>22/2/2222</td>
+                  {transactions.map( transaction => {
+                      return (
+                        <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>
+                            {new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                        }).format(transaction.amount)}</td>
+                        <td>{transaction.category}</td>
+                        <td>
+                        {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                        </td>
                     </tr>
-                    <tr>
-                        <td>ODSJFOJSDF</td>
-                        <td>1200</td>
-                        <td>SSASAD</td>
-                        <td>22/2/2222</td>
-                    </tr>
-                    <tr>
-                        <td>ODSJFOJSDF</td>
-                        <td>1200</td>
-                        <td>SSASAD</td>
-                        <td>22/2/2222</td>
-                    </tr>
+                      )
+                  })}
+               
                 </tbody>
             </table>
         </Container>
